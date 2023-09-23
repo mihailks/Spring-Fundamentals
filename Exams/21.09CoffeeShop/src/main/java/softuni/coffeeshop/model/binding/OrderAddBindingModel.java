8be438fe7aa3372811bd7d309b4eba20c5de0ecf
Mihail Slavov <mihail_slavov@icloud.com>
2023-09-23 18:57:20 +0300

    add order contr

--- Exams/21.09CoffeeShop/src/main/java/softuni/coffeeshop/model/binding/OrderAddBindingModel.java
+++ Exams/21.09CoffeeShop/src/main/java/softuni/coffeeshop/model/binding/OrderAddBindingModel.java
@@ -1,10 +1,68 @@
 package softuni.coffeeshop.model.binding;
 
+import jakarta.validation.constraints.*;
+import org.springframework.format.annotation.DateTimeFormat;
 import softuni.coffeeshop.model.entity.enums.CategoryNameEnum;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-public record OrderAddBindingModel(String name, BigDecimal price, LocalDateTime orderTime, CategoryNameEnum category,
-                                   String description) {
+public class OrderAddBindingModel {
+    private String name;
+    private BigDecimal price;
+    private LocalDateTime orderTime;
+    private CategoryNameEnum category;
+    private String description;
+
+    public OrderAddBindingModel() {
+    }
+
+    @Size(min = 3, max = 20)
+    @NotBlank
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Positive
+    @NotNull
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @PastOrPresent
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    public LocalDateTime getOrderTime() {
+        return orderTime;
+    }
+
+    public void setOrderTime(LocalDateTime orderTime) {
+        this.orderTime = orderTime;
+    }
+
+    @NotNull
+    public CategoryNameEnum getCategory() {
+        return category;
+    }
+
+    public void setCategory(CategoryNameEnum category) {
+        this.category = category;
+    }
+
+    @Size(min = 5)
+    @NotBlank
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- Exams/21.09CoffeeShop/src/main/java/softuni/coffeeshop/web/OrderController.java
+++ Exams/21.09CoffeeShop/src/main/java/softuni/coffeeshop/web/OrderController.java
@@ -1,14 +1,37 @@
 package softuni.coffeeshop.web;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import softuni.coffeeshop.model.binding.OrderAddBindingModel;
 
 @Controller
 @RequestMapping("/orders")
 public class OrderController {
     @GetMapping("/add")
-    public String addOrder(){
+    public String addOrder() {
         return "/order-add";
+    }
+
+    @PostMapping("/add")
+    public String addConfirm(@Valid OrderAddBindingModel orderAddBindingModel, BindingResult bindingResult,
+                             RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()){
+            redirectAttributes.addFlashAttribute("orderAddBindingModel",orderAddBindingModel)
+                    .addFlashAttribute("org.springframework.validation.BindingResult.orderAddBindingModel",bindingResult);
+            return "redirect:add";
+        }
+        //ADD to DB
+        return "redirect:/";
+    }
+
+    @ModelAttribute
+    public OrderAddBindingModel orderAddBindingModel() {
+        return new OrderAddBindingModel();
     }
 }
